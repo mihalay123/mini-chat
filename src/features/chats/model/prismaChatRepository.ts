c3d928827cea7e408d0bc9bcdfdc88163f368acc
mihalay123 <mihalay26@gmail.com>
2025-07-15 14:01:52 +0200

    refactor: remove error handling from createChat and getChatsByUserId methods

--- src/features/chats/model/prismaChatRepository.ts
+++ src/features/chats/model/prismaChatRepository.ts
@@ -3,59 +3,49 @@ import { ChatRepository } from './ChatRepository';
 
 export const prismaChatRepository: ChatRepository = {
   async createChat(userId, name = '', isGroup = false) {
-    try {
-      const chat = await prisma.chat.create({
-        data: {
-          name: isGroup ? name : null,
-          isGroup,
-          members: {
-            create: {
-              userId: userId,
-              role: isGroup ? 'ADMIN' : 'MEMBER',
-            },
+    const chat = await prisma.chat.create({
+      data: {
+        name: isGroup ? name : null,
+        isGroup,
+        members: {
+          create: {
+            userId: userId,
+            role: isGroup ? 'ADMIN' : 'MEMBER',
           },
         },
-        include: {
-          members: true,
-        },
-      });
+      },
+      include: {
+        members: true,
+      },
+    });
 
-      return chat;
-    } catch (error) {
-      console.error('Error creating chat:', error);
-      return null;
-    }
+    return chat;
   },
 
   async getChatsByUserId(userId) {
-    try {
-      const chats = await prisma.chat.findMany({
-        where: {
-          members: {
-            some: {
-              userId: userId,
-            },
+    const chats = await prisma.chat.findMany({
+      where: {
+        members: {
+          some: {
+            userId: userId,
           },
         },
-        include: {
-          members: {
-            where: { userId: userId },
-            select: { userId: true },
-          },
-          _count: {
-            select: { members: true },
-          },
-          messages: {
-            orderBy: { createdAt: 'desc' },
-            take: 1,
-          },
+      },
+      include: {
+        members: {
+          where: { userId: userId },
+          select: { userId: true },
         },
-      });
+        _count: {
+          select: { members: true },
+        },
+        messages: {
+          orderBy: { createdAt: 'desc' },
+          take: 1,
+        },
+      },
+    });
 
-      return chats;
-    } catch (error) {
-      console.error('Error getting chats:', error);
-      return [];
-    }
+    return chats;
   },
 };
